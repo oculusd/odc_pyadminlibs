6a906a863bc56676ff0721135e43167c570c6d6a
Nico Coetzee <info@oculusd.com>
2019-04-27 07:26:11 +0200

    Issue #7 - Updated license to LGPG

--- odc_pyadminlibs/actions/queries.py
+++ odc_pyadminlibs/actions/queries.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2018. All rights reserved. OculusD.com, Inc. 
+# This software is licensed under the LGPL license version 3 of 2007. A copy of
+# the license should be included with this software, usually in a file called
+# LICENSE.txt. If this is not the case, you can view the license online at
+# https://www.gnu.org/licenses/lgpl-3.0.txt
+
 from odc_pycommons import HOME, get_utc_timestamp
 from odc_pycommons.comms import get, json_post, get_service_uri, SERVICE_URIS
 from odc_pycommons.models import RootAccount, Thing, CommsRequest
